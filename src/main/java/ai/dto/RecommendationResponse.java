a4331834aafe5fd30f925beb26fc6a79f86a33d6
Raghavendra-Devale <raghavendradevale07@gmail.com>
2026-07-04 17:44:16 +0530

    feat: add data transfer objects for AI job recommendation requests and responses

---
+++ src/main/java/ai/dto/RecommendationResponse.java
@@ -0,0 +1,18 @@
+package ai.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class RecommendationResponse {
+    private List<RecommendationItem> recommendations;
+}
